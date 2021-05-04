a38ca5255d031d447bdb046bc5a751e047499a6d
bprall16 <77757244+bprall16@users.noreply.github.com>
2021-05-04 09:42:28 -0500

    Add files via upload

---
+++ turtleimage.py
@@ -0,0 +1,24 @@
+
+
+#Turtle image of stacked triangles
+
+import turtle
+
+Tut = turtle.Turtle()
+    
+Tut.pencolor("gold")
+Tut.forward(150)
+Tut.left(120)
+Tut.forward(300)
+Tut.left(120)
+Tut.forward(300)
+Tut.left(120)
+Tut.forward(150)
+Tut.left(60)
+Tut.forward(150)
+Tut.left(120)
+Tut.forward(150)
+Tut.left(120)
+Tut.forward(150)
+turtle.done()
+
