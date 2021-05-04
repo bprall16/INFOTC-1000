ec00f4e1ed101041bc607e4c2e5359260b286476
bprall16 <77757244+bprall16@users.noreply.github.com>
2021-05-04 09:37:59 -0500

    Add files via upload

---
+++ aboutme.py
@@ -0,0 +1,7 @@
+print("""
+Brandon Prall
+I am a 36 year old army combat veteran.
+I returned from Iraq in May of this year.
+I have a bachelors degree in aviation.
+I have never done programming before.
+""")
